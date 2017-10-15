9c4651a43e9a9bccd61571a186b990f6620e92b5
Eddy <yanto.eddy@gmail.com>
2017-10-15 11:44:44 +0800

    Resync with chapter 6 repo
    Loc : sails-js-in-action/chapter-6/27
    - Retained personal README

--- api/models/Video.js
+++ api/models/Video.js
@@ -1,9 +1,9 @@
 /**
- * Video.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
+* Video.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
 
 module.exports = {
 
